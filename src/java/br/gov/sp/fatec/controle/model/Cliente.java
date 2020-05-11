04ff145baaba47aa3878ac60f445b4da56581f3a
Marcelo Salustiano Silva <mrsalustiano@gmail.com>
2020-05-11 15:16:12 -0300

    Arquivos Daniela

--- src/java/br/gov/sp/fatec/controle/model/Cliente.java
+++ src/java/br/gov/sp/fatec/controle/model/Cliente.java
@@ -10,5 +10,60 @@ package br.gov.sp.fatec.controle.model;
  * @author re039859
  */
 public class Cliente {
+    private String nome;
+    private int cpf;
+    private int rg;
+    private String email;
+    private int telefone;
+    private String endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getRg() {
+        return rg;
+    }
+
+    public void setRg(int rg) {
+        this.rg = rg;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
     
 }
--- src/java/br/gov/sp/fatec/controle/model/Fornecedor.java
+++ src/java/br/gov/sp/fatec/controle/model/Fornecedor.java
@@ -10,5 +10,60 @@ package br.gov.sp.fatec.controle.model;
  * @author re039859
  */
 public class Fornecedor {
+    private String nome;
+    private String razao;
+    private int cnpj;
+    private String email;
+    private int telefone;
+    private String endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRazao() {
+        return razao;
+    }
+
+    public void setRazao(String razao) {
+        this.razao = razao;
+    }
+
+    public int getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(int cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
     
 }
