8bd3ea620ef3ed857dd31a0fac44d7917d68dcfc
Marcelo Salustiano Silva <mrsalustiano@gmail.com>
2020-05-11 13:58:23 -0300

    Inicio Projeto - montada estrutura basica

---
+++ src/java/br/gov/sp/fatec/controle/dao/Cliente.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.gov.sp.fatec.controle.dao;
+
+/**
+ *
+ * @author re039859
+ */
+public class Cliente {
+    
+}
